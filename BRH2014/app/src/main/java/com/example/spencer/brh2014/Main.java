4385e5f2c5cdad90cbee7a5ecc381ae0e8f1494d
agicsaki <ac2289@cornell.edu>
2014-09-27 20:49:14 -0400

    skeleton of alertDialog to change language

--- BRH2014/app/src/main/java/com/example/spencer/brh2014/Main.java
+++ BRH2014/app/src/main/java/com/example/spencer/brh2014/Main.java
@@ -1,83 +1,82 @@
 package com.example.spencer.brh2014;
 
-import com.example.spencer.brh2014.ShakeDetector;
-import com.example.spencer.brh2014.ShakeDetector.OnShakeListener;
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
-import com.example.spencer.brh2014.ShakeDetector.OnShakeListener;
 
+import com.example.spencer.brh2014.ShakeDetector.OnShakeListener;
 
 public class Main extends Activity implements OnShakeListener {
 
-    private Camera mCamera;
-    private CameraPreview mPreview;
-    private ShakeDetector shakeDetector;
+	private Camera mCamera;
+	private CameraPreview mPreview;
+	private ShakeDetector shakeDetector;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(android.R.layout.activity_main);
 
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-        
-       // Create a shake detector
-     	shakeDetector = new ShakeDetector(this);
+		// Create an instance of Camera
+		mCamera = getCameraInstance();
 
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
-        mPreview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mPreview.takePicture(Main.this);
-            }
-        });
-    }
+		// Create a shake detector
+		shakeDetector = new ShakeDetector(this);
 
+		// Create our Preview view and set it as the content of our activity.
+		mPreview = new CameraPreview(this, mCamera);
+		FrameLayout preview = (FrameLayout) findViewById(android.R.id.camera_preview);
+		preview.addView(mPreview);
+		mPreview.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				mPreview.takePicture(Main.this);
+			}
+		});
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(android.R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == android.R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-    
-    @Override
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	@Override
 	public void onShake() {
 		Toast.makeText(this, "shake detected", Toast.LENGTH_SHORT).show();
+		ChangeLanguageDialogBuilder dialogBuilder = new ChangeLanguageDialogBuilder(
+				getApplicationContext());
+		AlertDialog alert = dialogBuilder.getAlertDialog();
+		alert.show();
 	}
 }
